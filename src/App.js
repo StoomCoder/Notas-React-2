d85bad94f3fec88c1dee759061638df18069bb85
stoombasscode <cesarhuertaescalante@hotmail.com>
2019-06-14 14:02:11 -0500

    Reorganizacion de la estructura

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, {Component} from 'react';
 import './App.css';
-import Cards from './Cards/Cards.js';
+import { todos } from './todos.json'
+console.log(todos);
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  
+    this.state = {
+     
+      todos:todos
+  
+    }
+  }
 
   render(){
-
+    const todos = this.state.todos.map((todo, i) => {
 
     return (
-      <div className="App">
-
-      <Cards></Cards>
-    
-
-
-      </div>
+     
+     
+        <div className="container" key={i}>
+          <div className="row">
+            <div className="col-6">
+            <div className="card p">
+            <img
+            className="card-img-top" alt="Card image cap"src="https://picsum.photos/id/610/203/203"></img>
+            <div className="card-body">
+              <h5>{todo.Titulo}</h5>
+              <p>{todo.Contenido}</p>
+             <a className="btn btn-primary" href="https://picsum.photos/">Ver más</a>
+            </div>
+            </div>
+            </div>
+          </div>
+        </div>
     )
-  };
+  });
+
+  return (
+     
+    <div className="App">
+       <div className="">
+                <div className="row">
+                  {todos}
+                </div>
+              </div>
+    </div>
+  )
+
+
+
+    // eslint-disable-next-line no-unreachable
+   
+
+  }
 }
 
 
--- src/Cards/Cards.js
+++ src/Cards/Cards.js
@@ -1,40 +1,60 @@
 import React, {Component} from 'react';
-import { todos } from './todos.json'
+
+
 
 class Cards extends Component{
 
 
 
-
-
-
-
   render(){
+    
+    const todos = this.state.todos.map((todo,i) =>{
+
+      return(
+
+
+        <div>
+          <h1>Encabezado</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+            <div className="card p">
+            <img
+            className="card-img-top" alt="Card image cap"src="https://picsum.photos/id/610/203/203"></img>
+            <div className="card-body">
+              <h5>Titulo del proyecto</h5>
+              <p>En este proyecto podemos observar...</p>
+             <a className="btn btn-primary" href="https://picsum.photos/">Ver más</a>
+            </div>
+            </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      )
+    })
+
     return(
 
 
-      <div>
-        <h1>Encabezado</h1>
-
-
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-          <div className="card p">
-          <img
-          className="card-img-top"
-          alt="Card image cap"
-          src="https://picsum.photos/id/610/203/203"></img>
-          <div className="card-body">
-            <h5>Titulo del proyecto</h5>
-            <p>En este proyecto podemos observar...</p>
-           <a className="btn btn-primary" href="https://picsum.photos/">Ver más</a>
-          </div>
-          </div>
+<div>
+          <h1>Encabezado</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+            <div className="card p">
+            <img
+            className="card-img-top" alt="Card image cap"src="https://picsum.photos/id/610/203/203"></img>
+            <div className="card-body">
+              <h5>Titulo del proyecto</h5>
+              <p>En este proyecto podemos observar...</p>
+             <a className="btn btn-primary" href="https://picsum.photos/">Ver más</a>
+            </div>
+            </div>
+            </div>
           </div>
         </div>
-      </div>
-      </div>
+        </div>
 
 
 
@@ -43,5 +63,6 @@ class Cards extends Component{
     )
   }
 }
+
 
 export default Cards;
